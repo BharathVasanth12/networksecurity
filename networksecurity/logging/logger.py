cfe28119f5be30ce2a7c0ca6b91f72f2467e8158
BharathVasanth12 <bharath.vasanthkumar@gmail.com>
2025-08-20 17:12:09 +0530

    data ingestion

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -12,7 +12,7 @@ os.makedirs(logs_path, exist_ok=True)
 LOGS_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(module)s - %(levelname)s - %(message)s",
-                    level=logging.DEBUG,
+                    level=logging.INFO,
                     handlers=[logging.FileHandler(LOGS_FILE_PATH), logging.StreamHandler(sys.stdout)]
                 )
 
--- test_mongo_db_conn.py
+++ test_mongo_db_conn.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://bharathvasanthkumar:<Your password>@cluster0.cnwrcsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://bharathvasanthkumar:vmhjvjAh1jepuIfC@cluster0.cnwrcsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
